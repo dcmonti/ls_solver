05c1d2c324127edb1a75d3a79ef4d1af89f4acad
Samuele Campanella <s.campanella3@campus.unimib.it>
2023-05-07 22:13:50 +0200

    bindgen initial commit for c++ binding

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,19 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    cc::Build::new()
-        .file("src/sparse_matrix_product.c")
-        //.cpp_set_stdlib("stdc++")
-        .compile("sparse_matrix_product");
-}
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+    
+} 
